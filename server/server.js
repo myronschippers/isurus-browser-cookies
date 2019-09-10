b8bb046f0a33e757395a0fed343ff917d9e4af06
Myron <dubtwee@gmail.com>
2019-09-10 14:08:15 -0500

    stubbing in all of the cookie-session server API Endpoints

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 
 // App Set //
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// THIS IS THE MIDDLEWARE MOUNTING AREA
 
 //
 // EXPRESS APPLICATION MIDDLEWARE
@@ -12,7 +15,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// setup cookie-session as express middleware
+// create cookie session to be used on the `req` of he API routes
+app.use(cookieSession({
+    name: 'session',
+    keys: ['session'],
+    // how much time till cookie expires
+    maxAge: 2 * 60 * 1000 // 2 mins
+}));
 
 //
 // API ROUTES
@@ -20,12 +29,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // save the creature to cookie session
 app.post('/creature', (req, res) => {
-    // save creature sent
+    req.session.fantasticCreature = req.body.fantasticCreature;
+
+    res.sendStatus(200);
 });
 
 // retrieve the creature stored with cookie session
 app.get('/creature', (req,res) => {
-    // retrieve creature from cookie session
+    // if there is a call to get creature re-save creature to coockieSession otherwise set session to null 
+    req.session.fantasticCreature = req.session && req.session.fantasticCreature || null;
+
+    const {
+        fantasticCreature
+    } = req.session;
+
+    res.send({fantasticCreature});
 });
 
 /** Listen * */
